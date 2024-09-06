f776e5e6b2a96c2696cf72bcb78eee4143a773d6
Tran Minh Thanh <132430676+tranminhthanh24@users.noreply.github.com>
2024-09-06 11:13:38 +0700

    temp

---
+++ app/src/main/java/vn/edu/usth/nutritionrecipe/ui/explore/ExploreViewModel.java
@@ -0,0 +1,19 @@
+package vn.edu.usth.nutritionrecipe.ui.explore;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ExploreViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ExploreViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("LOAD MORE...");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
