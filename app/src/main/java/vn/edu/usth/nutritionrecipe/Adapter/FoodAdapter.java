1a1453f35afcb1607944cc82270156076f2141ed
Sith <104435872+NemoCaptain03@users.noreply.github.com>
2024-09-19 20:27:38 +0700

    minor fixing 2

--- app/src/main/java/vn/edu/usth/nutritionrecipe/Adapter/FoodAdapter.java
+++ app/src/main/java/vn/edu/usth/nutritionrecipe/Adapter/FoodAdapter.java
@@ -21,20 +21,14 @@ public class FoodAdapter extends ArrayAdapter<FoodList> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
-        FoodList listData = getItem(position); //Get the FoodList item at the specified position
-
-        //Check if the view is not created
+        FoodList listData = getItem(position);
         if (view == null){
-            //Inflate the layout
             view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_explore_list_items, parent, false);
         }
-
-        //Find ImageView and TextView from the resources ID
         ImageView listImage = view.findViewById(R.id.listImage);
         TextView listName = view.findViewById(R.id.listName);
         TextView listTime = view.findViewById(R.id.listTime);
 
-        //Set the data for each views
         listImage.setImageResource(listData.image);
         listName.setText(listData.name);
         listTime.setText(listData.time);
